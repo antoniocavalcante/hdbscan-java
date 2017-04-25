766a515b8fd1310c27720a28debad0405774eb5b
Toni Cavalcante <ant.cavalcante@gmail.com>
2017-04-24 18:06:27 -0600

    core-distances experiment

--- experiments/coredistances.sh
+++ experiments/coredistances.sh
@@ -2,36 +2,24 @@
 
 DIR="data#6"
 
-# rm -rf *.results
-
 dataset() {
     for n in 16 32 64 256 512 1024;
     do
-        java -jar coredistances.jar "${DIR}/16d-${n}.dat" 16
+        java -jar coredistances.jar "${DIR}/16d-${n}.dat" 16 >> core-distances.result
     done
 }
 
 minpoints() {
-    java -jar coredistances.jar "${DIR}/16d-128.dat" 128
+    java -jar coredistances.jar "${DIR}/16d-128.dat" 128 >> core-distances.result
 }
 
 dimensions() {
     for d in 2 4 8 32 64 128;
     do
-        java -jar coredistances.jar "${DIR}/${d}d-128.dat" 16
+        java -jar coredistances.jar "${DIR}/${d}d-128.dat" 16 >> core-distances.result
     done
 }
 
-option="${1}" 
-
-case ${option} in
-   1) echo "dimensions"
-      dimensions
-      ;; 
-   2) echo "dataset"
-      dataset
-      ;;
-   3) echo "minpoints"
-      minpoints
-      ;; 
-esac
+dataset
+minpoints
+dimensions
